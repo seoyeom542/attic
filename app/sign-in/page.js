113ca5437fb4f1499ce7975881b1af16a801bf9b
seoyeom542 <seoyeom542@gmail.com>
2024-10-06 15:01:38 +0900

    create sign-in

---
+++ app/sign-in/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function SignIn() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        로그인 화면임
+    </div>
+  );
+}
